e7af253b8468d2010669c1065c81313c82afbae2
Devil7DK <dineshthangavel47@gmail.com>
2020-10-04 22:23:10 +0530

    Replace obfuscated integers with actual values

--- Extracted.js
+++ Extracted.js
@@ -1,48 +1,47 @@
 var Keys = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
 function Decrypt(value) {
-    var n = value.substr(-10 * -113 + 4455 * 2 + 2 * -5020, -319 + 6171 + -5843)
-        , i = (-4801 * -1 + 233 * 28 + 453 * -25,
-            f1)(value.substr(-5222 + -4874 + 235 * 43));
+    var n = value.substr(0, 9)
+        , i = f1(value.substr(9));
     try {
         i = decodeURIComponent(i)
-    } catch (value) { }
+    } catch (err) { }
     return f2(n, i)
 }
 
 function f1(value) {
-        if ((value = (value = "".concat(value)).replace(/[ \t\n\f\r]/g, "")).length % (2967 * -1 + 851 * 2 + 9 * 141) == -6094 + -1 * 3079 + 9173 && (value = value.replace(/==?$/, "")),
-        value.length % (-2237 * 2 + 1 * 9529 + -5051) == -8061 * -1 + -6531 + -1529 || /[^+/0-9A-Za-z]/.test(value))
-            return null;
-        for (var x, r, u = "", c = -1152 * -6 + 2811 + -9723, e = 5 * 467 + -549 + -1786, f = 1923 + -844 + -1079; f < value.length; f++)
-            c <<= 456 + -5455 + 1001 * 5,
+    if ((value = (value = "".concat(value)).replace(/[ \t\n\f\r]/g, "")).length % 4 == 0 && (value = value.replace(/==?$/, "")),
+        value.length % 4 == 1 || /[^+/0-9A-Za-z]/.test(value))
+        return null;
+    for (var x, r, u = "", c = 0, e = 0, f = 0; f < value.length; f++)
+        c <<= 6,
             c |= (x = value[f],
-            r = void (4540 * -1 + -304 + 7 * 692),
-            (r = Keys.indexOf(x)) < 2183 + -2 * -1627 + -5437 ? void (3207 + -230 + -2977) : r),
-            -2816 + 1 * -7438 + 10278 === (e += -4 * 728 + 2366 * 1 + 552) && (u += String.fromCharCode((-1 * 25541795 + 60905 * -293 + -1660 * -36204 & c) >> 163 * -14 + 4751 * 2 + 4 * -1801),
-            u += String.fromCharCode((120469 * -1 + 102168 + 53 * 1577 & c) >> -3 * 287 + -15 * -335 + -4156),
-            u += String.fromCharCode(((1533 + -1 * -9353 + -1 * 10631) & c)),
-            c = e = 191 * -43 + -2503 + 10716);
-        return 15 * -344 + 3 * 72 + -826 * -6 === e ? (c >>= -8120 + -9417 + 17541,
-        u += String.fromCharCode(c)) : 9920 + 178 * -29 + -4740 === e && (c >>= 1457 + -107 * 3 + -1134,
-        u += String.fromCharCode((-129312 + 7155 + 129 * 1453 & c) >> 6 * -639 + 3 * -2586 + 11600),
-        u += String.fromCharCode(((7089 + 2181 + -9015)& c))),
+                r = void (0),
+                (r = Keys.indexOf(x)) < 0 ? void (0) : r),
+            24 === (e += 6) && (u += String.fromCharCode((16711680 & c) >> 16),
+                u += String.fromCharCode((65280 & c) >> 8),
+                u += String.fromCharCode((255 & c)),
+                c = e = 0);
+    return 12 === e ? (c >>= 4,
+        u += String.fromCharCode(c)) : 18 === e && (c >>= 2,
+            u += String.fromCharCode((65280 & c) >> 8),
+            u += String.fromCharCode(((255) & c))),
         u
 }
 
 function f2(t, n) {
-    for (var x, r = [], u = -1355 + 5448 + -4093 * 1, c = "", e = -9878 * -1 + 1223 + -10845, f = 2211 + -1 * -6983 + 4597 * -2; f < e; f += 1053 * -1 + 5298 + -4244)
+    for (var x, r = [], u = 0, c = "", e = 256, f =0; f < e; f += 1)
         r[f] = f;
-    for (f = 4632 + -5453 + -821 * -1; f < e; f += -106 * 77 + 6 * -293 + -1 * -9921)
+    for (f = 0; f < e; f += 1)
         u = ((u + r[f] + t.charCodeAt(f % t.length)) % e),
-        x = r[f],
-        r[f] = r[u],
-        r[u] = x;
-    for (var s = u = f = -1 * -9164 + 6306 + -15470; s < n.length; s += -9852 + -201 * 38 + 17491 * 1)
-        u = (u + r[f = (f + (-7933 * -1 + -5790 + -2142)) % e]) % e,
-        x = r[f],
-        r[f] = r[u],
-        r[u] = x,
-        c += String.fromCharCode(n.charCodeAt(s) ^ r[(r[f] + r[u]) % e]);
+            x = r[f],
+            r[f] = r[u],
+            r[u] = x;
+    for (var s = u = f = 0; s < n.length; s += 1)
+        u = (u + r[f = (f + 1) % e]) % e,
+            x = r[f],
+            r[f] = r[u],
+            r[u] = x,
+            c += String.fromCharCode(n.charCodeAt(s) ^ r[(r[f] + r[u]) % e]);
     return c
 }
